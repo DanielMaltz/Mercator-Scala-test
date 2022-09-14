18f2ba21741ca52a639e2ab75bbe99fec71e5fd9
Daniel <daniel.maltz@omt.org.uk>
2022-09-14 18:43:33 +0100

    Offers incorparated

--- MercatorTest.scala
+++ MercatorTest.scala
@@ -7,6 +7,8 @@ object MercatorTest{
 		Console.printf(output.toString())
 		output = totalCost(Array("Apple", "Other", "Orange"))
 		Console.printf(output.toString())
+		output = totalCost(Array("Orange", "Orange", "Apple", "Apple", "Orange"))
+		Console.printf(output.toString())
 	}
 
 	def totalCost(list: Array[String]) : Double = {
@@ -20,7 +22,14 @@ object MercatorTest{
 
 			count = count+1
 		}
-		if (invalidEntry) -1 else apples*0.6+oranges*0.25
+		if (invalidEntry) -1 else {
+			if (apples%2 == 0) apples = apples/2
+			else {apples = apples-1; apples = apples/2; apples = apples+1}
+
+			val remainder = oranges%3
+			oranges = 2 * ((oranges-remainder)/3) + remainder
+			apples*0.6+oranges*0.25
+		}
 	}
 
 }
